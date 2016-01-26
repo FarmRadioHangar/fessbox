12fb8d0a6c32e065bb7fc449ec2029cbc6dc6c72
Johannes Hilden <hildenjohannes@gmail.com>
2016-01-26 17:30:56 +0300

    refactor Host component

--- frontend/modules/Host.js
+++ frontend/modules/Host.js
@@ -1,107 +1,49 @@
 import React  from 'react'
 import Slider from './Slider'
 
-import { updateUserLevel, updateLevel }
-  from '../js/actions'
-
-class SliderBar extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    const { 
-      icon, 
-      muted, 
-      value,
-      defaultValue, 
-      onChange, 
-      onToggleMuted 
-    } = this.props
-    return (
-      <div className='panel panel-default'>
-        <div className='panel-body' style={styles.panel}>
-          <Slider 
-            value        = {value} 
-            defaultValue = {defaultValue} 
-            onChange     = {onChange}
-            enabled      = {!muted}
-            orientation  = 'vertical'
-            reversed     = {true}
-            min          = {1}
-            max          = {100} />
-        </div>
-        <div style={styles.buttonWrapper}> 
-          <button 
-            style        = {styles.button} 
-            className    = 'btn btn-default btn-sm' 
-            onClick      = {onToggleMuted}>
-            <i className = {`glyphicon glyphicon-volume-${muted ? 'off' : 'up'}`} />
-          </button>
-        </div>
-        <div style={styles.iconWrapper}> 
-          <i className={`fa fa-${icon}`} />
-        </div>
-      </div>
-    )
-  }
-}
-
 class Host extends React.Component {
   constructor(props) {
     super(props)
   }
-  setChannelMuted(muted) {
-    const { sendMessage, client } = this.props
-    sendMessage('channelMuted', {
-      [client.userId] : muted
-    })
+  toggleMuted() {
+    const { muted, sendMessage } = this.props
+    sendMessage('hostMuted', !muted)
   }
-  updateChannelLevel(level) {
-    const { sendMessage, client, dispatch } = this.props
-    sendMessage('channelVolume', {
-      [client.userId] : level
-    })
-    dispatch(updateLevel(client.userId, level))
-  }
-  setUserMuted(muted) {
-    const { sendMessage, client } = this.props
-    sendMessage('userMuted', {
-      [client.userId] : muted 
-    })
-  }
-  updateUserLevel(level) {
-    const { sendMessage, client, dispatch } = this.props
-    sendMessage('userVolume', {
-      [client.userId] : level 
-    })
-    dispatch(updateUserLevel(client.userId, level))
+  updateLevel(event) {
+    const { dispatch, sendMessage } = this.props
+    const value = event.target ? event.target.value : event
+    sendMessage('hostVolume', value)
   }
   render() {
-    const { client, mixer, users } = this.props
-    const user = users[client.userId]
-    const channel = mixer.channels[client.userId]
-    if (!channel || !user) {
-      return <span />
-    }
+    const { level, muted, active } = this.props
     return (
-      <div style={styles.wrapper}>
-        <div style={styles.bar}> 
-          <SliderBar 
-            icon          = 'microphone' 
-            value         = {channel.level}
-            defaultValue  = {channel.level}
-            muted         = {channel.muted}
-            onChange      = {(from, to) => { this.updateChannelLevel(to) }}
-            onToggleMuted = {() => { this.setChannelMuted(!channel.muted) }} />
-        </div>
-        <div style={{...styles.bar, margin: '11px'}}> 
-          <SliderBar 
-            icon          = 'headphones' 
-            value         = {user.level}
-            defaultValue  = {user.level}
-            muted         = {user.muted}
-            onChange      = {(from, to) => { this.updateUserLevel(to) }}
-            onToggleMuted = {() => { this.setUserMuted(!user.muted) }} />
+      <div className='panel panel-default' style={{
+        ...styles.panel,
+        backgroundColor : active ? '#f04124' : 'transparent'
+      }}>
+        <div className='panel-body'>
+          <div>
+            <div style={styles.sliderWrapper}> 
+              <Slider 
+                orientation  = 'vertical'
+                reversed     = {true}
+                min          = {1}
+                max          = {100}
+                defaultValue = {level}
+                value        = {level}
+                enabled      = {!muted}
+                onChange     = {(from, to) => { this.updateLevel(to) }} />
+            </div>
+          </div>
+          <div style={styles.buttonWrapper}> 
+            <button 
+              style          = {styles.button} 
+              className      = 'btn btn-default btn-sm' 
+              onClick        = { () => this.toggleMuted() }>
+              <i className={`glyphicon glyphicon-volume-${muted ? 'off' : 'up'}`} />
+            </button>
+          </div>
+          <div style={styles.label}>H</div>
         </div>
       </div>
     )
@@ -109,29 +51,22 @@ class Host extends React.Component {
 }
 
 const styles = {
-  panel : { 
-    textAlign    : 'center'
+  buttonWrapper : {
+    textAlign    : 'center', 
+    margin       : '12px 0'
   },
   button : {
     lineHeight   : 1.9, 
     borderRadius : '50% 50%'
   },
-  buttonWrapper : {
-    textAlign    : 'center', 
-    margin       : '0 0 12px 0'
+  label : {
+    textAlign    : 'center'
   },
-  iconWrapper : {
-    textAlign    : 'center', 
-    margin       : '0 0 12px', 
-    fontSize     : '150%'
+  panel : {
+    margin: '11px 11px 11px 0'
   },
-  wrapper : {
-    display      : 'flex'
-  },
-  bar : {
-    flex         : 1, 
-    minWidth     : '80px', 
-    margin       : '11px 6px 11px 11px'
+  sliderWrapper : {
+    textAlign: 'center'
   },
 }
 
