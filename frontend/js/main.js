13f22be37101ff0eda27e6582a19aa6b0f6ab62f
Johannes Hilden <hildenjohannes@gmail.com>
2016-01-26 17:38:16 +0300

    bootstrap app when initialization event arrives

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -13,16 +13,16 @@ import { compose, createStore }
   from 'redux'
 import { Provider, connect } 
   from 'react-redux'
-import { initializeMixer, initializeUsers, updateUser, updateMixer, updateMaster, updateMasterLevel, updateLevel, setTimeDiff, updateCaller, updateInbox, removeMessage }
+import { initializeMixer, initializeUsers, updateUser, removeUser, updateMixer, updateMaster, updateMasterLevel, updateLevel, setTimeDiff, updateCaller, updateInbox, removeMessage }
   from './actions'
 
-const userId   = getQueryVariable('user_id') || 701
+const userId   = getQueryVariable('user_id') 
 const hostUrl  = getQueryVariable('host_url') || 'fessbox.local:19998' // '192.168.1.38:19998'
 const language = getQueryVariable('language') || 'en' 
 
-const createPersistentStore = compose(persistState('client', {key: `__fessbox_client_${userId}`}))(createStore)
-const store = createPersistentStore(app, {client: {userId, channels: {}, $: Math.random()*1000000000|0}})
 const ws = new ReconnectingWebSocket(`ws://${hostUrl}/?user_id=${userId}`) 
+
+let store = null
 
 class App extends React.Component {
   constructor(props) {
@@ -46,50 +46,82 @@ class App extends React.Component {
   }
 }
 
-injectTapEventPlugin()
+function initApp(data) {
 
-i18next.init({
-  lng: language,
-  resources: {
-    se: {
-      translation: {
-        'Incoming call' : 'Inkommande samtal',
-        'Incoming SMS'  : 'Inkommande SMS',
-        'Outgoing SMS'  : 'Utgående SMS',
-        'Inbox'         : 'Inkorg',
-        'Type'          : 'Typ',
-        'Time'          : 'Tidpunkt',
-        'Sender'        : 'Avsändare',
-        'Content'       : 'Innehåll',
-        'Free line'     : 'Tillgänglig linje',
-        'Host'          : 'Värd',
-        'Private'       : 'Privat',
-        'Master'        : 'Huvudkanal',
-        'On hold'       : 'Parkera',
-        'IVR'           : 'Röstbrevlåda',
-        'Defunct'       : 'Ur funktion',
-        'Edit caller'   : 'Personinformation',
-        'Hang up'       : 'Avsluta samtal',
-        'Edit caller details' 
-                        : 'Uppdatera personinformation',
-        'Name'          : 'Namn',
-        'Location'      : 'Plats',
-        'Save'          : 'Spara',
-        'Cancel'        : 'Avbryt',
+  if (!store) {
+    const createPersistentStore = compose(persistState('client', {
+      key    : `__fessbox_client_${userId}`,
+      slicer : function(paths) {
+        return state => {
+          const { channels, diff, $ } = state.client
+          return { client : { channels, diff, $ } }
+        }
+      }
+    }))(createStore)
+    store = createPersistentStore(app, {client: {channels: {}, $: Math.random()*1000000000|0}})
+  }
+  
+  store.dispatch(initializeMixer(data.mixer))
+  if (data.users) {
+    store.dispatch(initializeUsers(data.users))
+  }
+  // Compute time difference between server and device
+  const diff = Date.now() - data.server_time
+  console.log(`diff : ${diff}`)
+  store.dispatch(setTimeDiff(diff))
+  // Initialize message inbox
+  if (data.inbox) {
+    data.inbox.ids.slice().reverse().forEach(id => {
+      store.dispatch(updateInbox(id, data.inbox.messages[id]))
+    })
+  }
+
+  injectTapEventPlugin()
+  
+  i18next.init({
+    lng: language,
+    resources: {
+      se: {
+        translation: {
+          'Incoming call' : 'Inkommande samtal',
+          'Incoming SMS'  : 'Inkommande SMS',
+          'Outgoing SMS'  : 'Utgående SMS',
+          'Inbox'         : 'Inkorg',
+          'Type'          : 'Typ',
+          'Time'          : 'Tidpunkt',
+          'Sender'        : 'Avsändare',
+          'Content'       : 'Innehåll',
+          'Free line'     : 'Tillgänglig linje',
+          'Host'          : 'Värd',
+          'Private'       : 'Privat',
+          'Master'        : 'Huvudkanal',
+          'On hold'       : 'Parkera',
+          'IVR'           : 'Röstbrevlåda',
+          'Defunct'       : 'Ur funktion',
+          'Edit caller'   : 'Personinformation',
+          'Hang up'       : 'Avsluta samtal',
+          'Edit caller details' 
+                          : 'Uppdatera personinformation',
+          'Name'          : 'Namn',
+          'Location'      : 'Plats',
+          'Save'          : 'Spara',
+          'Cancel'        : 'Avbryt',
+        }
       }
     }
-  }
-}, (err, t) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App t={t} />
-    </Provider>,
-    document.getElementById('main')
-  )
-})
+  }, (err, t) => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App t={t} />
+      </Provider>,
+      document.getElementById('main')
+    )
+  })
 
-ws.onopen  = () => { console.log('open') } 
-ws.onclose = () => { console.log('close') } 
+}
+
+ws.onopen  = () => { console.log('WebSocket connection established.') } 
+ws.onclose = () => { console.log('WebSocket connection closed.') } 
 
 ws.onmessage = e => { 
   if (e.data) {
@@ -97,67 +129,63 @@ ws.onmessage = e => {
     console.log('>>> Message')
     console.log(msg)
     console.log('<<<')
-    switch (msg.event) {
-      case 'initialize':
-        store.dispatch(initializeMixer(msg.data.mixer))
-        /* store.dispatch(initializeMixer(temp)) */
-        if (msg.data.users) {
-          store.dispatch(initializeUsers(msg.data.users))
-        }
-        // Compute time difference between server and device
-        const diff = Date.now() - msg.data.server_time
-        console.log(`diff : ${diff}`)
-        store.dispatch(setTimeDiff(diff))
-        // Initialize message inbox
-        if (msg.data.inbox) {
-          msg.data.inbox.ids.slice().reverse().forEach(id => {
-            store.dispatch(updateInbox(id, msg.data.inbox.messages[id]))
+    if ('initialize' == msg.event) {
+      initApp(msg.data)
+    } else if (store) {
+      switch (msg.event) {
+        case 'channelUpdate':
+          store.dispatch(updateMixer(msg.data))
+          Object.keys(msg.data).forEach(chan => {
+            if (!msg.data[chan]) {
+              store.dispatch(removeUser(chan)) 
+            }
           })
-        }
-        break
-      case 'channelUpdate':
-        store.dispatch(updateMixer(msg.data))
-        break
-      case 'channelVolumeChange':
-        Object.keys(msg.data).forEach(chan => {
-          store.dispatch(updateLevel(chan, msg.data[chan])) 
-        })
-        break
-      case 'userUpdate':
-        Object.keys(msg.data).forEach(user => {
-          store.dispatch(updateUser(user, msg.data[user])) 
-        })
-        break
-      case 'masterUpdate':
-        store.dispatch(updateMaster(msg.data))
-        break
-      case 'masterVolumeChange':
-        store.dispatch(updateMasterLevel(msg.data))
-        break
-      case 'channelContactInfo':
-        Object.keys(msg.data).forEach(chan => {
-          store.dispatch(updateCaller(chan, msg.data[chan]))
-        })
-        break
-      case 'inboxUpdate':
-        Object.keys(msg.data).forEach(id => {
-          if (msg.data[id]) {
-            store.dispatch(updateInbox(id, msg.data[id]))
-          } else {
-            store.dispatch(removeMessage(id))
-          }
-        })
-        break
-      case 'inboxMessages':
-        // @TODO
-        break
-      default:
-        break
+          break
+        case 'channelVolumeChange':
+          Object.keys(msg.data).forEach(chan => {
+            store.dispatch(updateLevel(chan, msg.data[chan])) 
+          })
+          break
+        case 'userUpdate':
+          Object.keys(msg.data).forEach(user => {
+            //if (msg.data[user]) {
+            store.dispatch(updateUser(user, msg.data[user])) 
+            //} else {
+            //  store.dispatch(removeUser(user)) 
+            //}
+          })
+          break
+        case 'masterUpdate':
+          store.dispatch(updateMaster(msg.data))
+          break
+        case 'masterVolumeChange':
+          store.dispatch(updateMasterLevel(msg.data))
+          break
+        case 'channelContactInfo':
+          Object.keys(msg.data).forEach(chan => {
+            store.dispatch(updateCaller(chan, msg.data[chan]))
+          })
+          break
+        case 'inboxUpdate':
+          Object.keys(msg.data).forEach(id => {
+            if (msg.data[id]) {
+              store.dispatch(updateInbox(id, msg.data[id]))
+            } else {
+              store.dispatch(removeMessage(id))
+            }
+          })
+          break
+        case 'inboxMessages':
+          // @TODO
+          break
+        default:
+          break
+      }
     }
   }
 } 
 
 ws.onerror = e => { 
-  console.log('error')
+  /*alert('Connection to server ' + hostUrl + ' failed.')*/
   console.log(e)
 }
