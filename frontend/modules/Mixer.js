5972f5c21dfc437c9116c68e3749fcaeb86a03a9
Johannes Hilden <hildenjohannes@gmail.com>
2016-01-27 12:32:20 +0300

    add missing connected prop to Channel

--- frontend/modules/Mixer.js
+++ frontend/modules/Mixer.js
@@ -69,7 +69,7 @@ class Mixer extends React.Component {
       inbox,
       dispatch, 
       sendMessage,
-      users : { _userId },
+      users : { _userId, _connected },
       t,
     } = this.props
     return (
@@ -86,6 +86,7 @@ class Mixer extends React.Component {
                   t           = {t}
                   key         = {id}
                   channelId   = {id}
+                  isConnected = {_connected}
                   client      = {client}
                   dispatch    = {dispatch}
                   sendMessage = {sendMessage} />
