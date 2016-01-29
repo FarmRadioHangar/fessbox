65985541a437c3197b2e3867c4b49a20cb09ce80
Damjan Janevski <miopa2@gmail.com>
2016-01-29 12:43:45 +0300

    heartbeat moved to reverse tunnel script

--- backend-node/sh/check_server.sh
+++ backend-node/sh/check_server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#heartbeat
-wget -q -O /dev/null --no-check-certificate https://triple-a:test22@thehangar.farmradio.org:669
-
 PIDFILE=run/fessbox.pid
 PID=`cat $PIDFILE`
 NODEPATH=run/node_fessbox
@@ -11,3 +8,4 @@ if ! kill -0 $PID > /dev/null 2>&1; then
   $NODEPATH index.js >> log/application.log 2>> log/debug.log &
   echo $! > $PIDFILE
 fi
+
