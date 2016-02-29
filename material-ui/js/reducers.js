cdf6cd9a4382625ebc43ff9d0db865df0670e890
Johannes Hilden <hildenjohannes@gmail.com>
2016-02-29 13:33:15 +0300

    implement main reducer

---
+++ material-ui/js/reducers.js
@@ -0,0 +1,14 @@
+//import ui    from './reducers/ui'
+//import mixer from './reducers/mixer'
+//import inbox from './reducers/inbox'
+//import users from './reducers/users'
+
+import { combineReducers } 
+  from 'redux'
+
+export default combineReducers({
+//  ui,
+//  mixer,
+//  inbox,
+//  users,
+}
