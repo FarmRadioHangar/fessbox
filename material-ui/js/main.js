828d2884cef11ed1a33d312ebc55f49525683d44
Johannes Hilden <hildenjohannes@gmail.com>
2016-02-29 13:32:17 +0300

    boilerplate

---
+++ material-ui/js/main.js
@@ -0,0 +1,43 @@
+import 'babel-polyfill'
+import React            from 'react'
+import ReactDOM         from 'react-dom'
+import reducers         from './reducers'
+
+import injectTapEventPlugin 
+  from 'react-tap-event-plugin'
+import { ReconnectingWebSocket } 
+  from 'awesome-websocket'
+import { createStore } 
+  from 'redux'
+import { Provider } 
+  from 'react-redux'
+
+injectTapEventPlugin()
+
+const ws = new ReconnectingWebSocket(`ws://${hostUrl}/?user_id=${userId}`) 
+const store = createStore(reducers, {})
+
+ws.onopen  = () => { console.log('WebSocket connection established.') } 
+ws.onclose = () => { console.log('WebSocket connection closed.') } 
+
+ws.onmessage = e => { 
+}
+
+ws.onerror = e => { 
+  console.error(e)
+  //store.dispatch(updateUiStatus(UI_STATUS_ERROR, 'Error establishing WebSocket connection.'))
+}
+
+function sendMessage(type, data) {
+  ws.send(JSON.stringify({
+    event: type, 
+    data
+  }))
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Ui sendMessage={sendMessage} />
+  </Provider>,
+  document.getElementById('main')
+)
