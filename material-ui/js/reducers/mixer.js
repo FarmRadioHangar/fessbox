742bbb14ef3c4d80f0ed61619203e5924bc0a24e
Johannes Hilden <hildenjohannes@gmail.com>
2016-02-29 23:31:55 +0300

    implement app initialize action in mixer reducer

--- material-ui/js/reducers/mixer.js
+++ material-ui/js/reducers/mixer.js
@@ -2,11 +2,26 @@ import {
   APP_INITIALIZE, 
 } from '../constants'
 
-const initialState = {}
+const initialState = {
+  channelList  : [],
+}
+
+function channelList(channels) {
+  return Object.entries(channels).map(([id, chan]) => {
+    return {
+      id,
+      ...chan,
+    }
+  })
+}
 
 function reducer(state = initialState, action) {
   switch (action.type) {
     case APP_INITIALIZE:
+      return {
+        ...action.data.mixer,
+        channelList : channelList(action.data.mixer.channels),
+      }
     default:
       return state
   }
