74d705ad5e62b2a28b633ee55f29af85481e9268
Johannes Hilden <hildenjohannes@gmail.com>
2016-02-29 13:53:37 +0300

    add app reducer

--- material-ui/js/reducers.js
+++ material-ui/js/reducers.js
@@ -1,6 +1,6 @@
-//import ui    from './reducers/ui'
-//import mixer from './reducers/mixer'
 //import inbox from './reducers/inbox'
+//import mixer from './reducers/mixer'
+import app   from './reducers/app'
 import users from './reducers/users'
 
 import { combineReducers } 
@@ -10,5 +10,6 @@ export default combineReducers({
 //  ui,
 //  mixer,
 //  inbox,
+  app,
   users,
 })
