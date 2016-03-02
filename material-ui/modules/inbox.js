1cc22ec3424597c900b282fce47104020f3d7c55
Johannes Hilden <hildenjohannes@gmail.com>
2016-03-02 12:02:37 +0300

    show messages in inbox

--- material-ui/modules/inbox.js
+++ material-ui/modules/inbox.js
@@ -1,16 +1,65 @@
-import React from 'react'
+import React   from 'react'
+import TimeAgo from 'react-timeago'
+
+import { connect } 
+  from 'react-redux'
+
+import List 
+  from 'material-ui/lib/lists/list'
+import ListItem 
+  from 'material-ui/lib/lists/list-item'
+import Divider 
+  from 'material-ui/lib/divider'
+import Subheader 
+  from 'material-ui/lib/Subheader'
+
+import { grey400, darkBlack, lightBlack } 
+  from 'material-ui/lib/styles/colors'
+
+function messageType(key, read) {
+  if ('sms_in' == key) {
+    return `${read ? 'Incoming' : 'New incoming'} SMS from`
+  } else {
+    return 'Outgoing SMS to'
+  }
+}
 
 class Inbox extends React.Component {
   constructor(props) {
     super(props)
   }
   render() {
+    const { inbox : { visibleMessages } } = this.props
     return (
-      <div>
-        Inbox
-      </div>
+      <List>
+        <Subheader>Messages</Subheader>
+        {visibleMessages.map(message => (
+          <div>
+            <Divider />
+            <ListItem
+              key                = {message.id}
+              primaryText        = {`${messageType(message.type, message.read)} ${message.source}`}
+              secondaryTextLines = {2}
+              secondaryText      = {
+                <p>
+                  {!isNaN(message.timestamp) && (
+                    <span style={{color: darkBlack}}>
+                      <TimeAgo date={Number(message.timestamp)} /> &mdash;&nbsp;
+                    </span> 
+                  )}
+                  {message.content}
+                </p>
+              }
+            />
+          </div>
+        ))}
+      </List>
     )
   }
 }
 
-export default Inbox
+const InboxComponent = connect(state => ({
+  inbox : state.inbox,
+}))(Inbox)
+
+export default InboxComponent
