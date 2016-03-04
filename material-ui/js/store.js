d50b34cedf10bbabc2fdc2fe8bc71ecde55cdce7
Johannes Hilden <hildenjohannes@gmail.com>
2016-03-04 12:15:39 +0300

    add  LiveReactload hack to store

--- material-ui/js/store.js
+++ material-ui/js/store.js
@@ -5,4 +5,15 @@ import { createStore }
 
 const store = createStore(reducers, {})
 
+/* 
+ * LiveReactload hack 
+ */
+if (module.onReload) {
+  module.onReload(() => {
+    const nextReducer = require('./reducers')
+    store.replaceReducer(nextReducer.default || nextReducer)
+    return true
+  })
+}
+
 export default store
