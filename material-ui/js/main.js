7f7a98e9988ddace9c430dbc5721b5ee5556c4be
johanneshilden <hildenjohannes@gmail.com>
2016-04-05 12:13:25 +0300

    simplify code

--- material-ui/js/main.js
+++ material-ui/js/main.js
@@ -18,7 +18,7 @@ import { updateAppStatus }
   from './actions'
 
 const userId   = getUrlParam('user_id') 
-const hostUrl  = getUrlParam('host_url') || 'fessbox.local:19998' // '192.168.1.38:19998'
+const hostUrl  = getUrlParam('host_url') || 'fessbox.local:19998' 
 const language = getUrlParam('language') || 'en' 
 
 const ws = new ReconnectingWebSocket(`ws://${hostUrl}/?user_id=${userId}`) 
@@ -60,18 +60,11 @@ ws.onerror = e => {
   store.dispatch(updateAppStatus(APP_STATUS_ERROR, 'Error establishing WebSocket connection.'))
 }
 
-function sendMessage(type, data) {
-  ws.send(JSON.stringify({
-    event : type, 
-    data
-  }))
-}
-
 injectTapEventPlugin()
 
 ReactDOM.render(
   <Provider store={store}>
-    <Ui sendMessage={sendMessage} />
+    <Ui sendMessage={(event, data) => ws.send(JSON.stringify({event, data}))} />
   </Provider>,
   document.getElementById('main')
 )
