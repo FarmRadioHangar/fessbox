2b4b3381b3b4b1b435ec77e0b658ddbef90cb491
johanneshilden <hildenjohannes@gmail.com>
2016-04-22 12:58:23 +0300

    add initial object to mixer reducer for testing

--- material-ui/js/reducers/mixer.js
+++ material-ui/js/reducers/mixer.js
@@ -1,11 +1,150 @@
 import { 
   APP_INITIALIZE, 
   CHANNEL_UPDATE,
+  CHANNEL_CONTACT_UPDATE,
   CHANNEL_VOLUME_UPDATE,
 } from '../constants'
 
+//const initialState = {
+//  channelList : [],
+//}
+
+
 const initialState = {
-  channelList : [],
+  "channels": {
+    "airtel1": {
+      "type": "Dongle",
+      "level": 56,
+      "direction": "incoming",
+      "label": "+255689514544",
+      "mode": "ring",
+      "muted": false,
+      "timestamp": 1461056646818,
+      "autoanswer": null,
+      "contact": {
+        "name": "johannes",
+        "number": "+255678647268"
+      },
+      "recording": false,
+      "number": "+255689514544",
+      "error": "not connected"
+    },
+    "tigo1": {
+      "type": "Dongle",
+      "level": 56,
+      "direction": "incoming",
+      "label": "+255689514544",
+      "mode": "master",
+      "muted": false,
+      "timestamp": 1461066884306,
+      "autoanswer": null,
+      "contact": {
+        "name": "johannes",
+        "number": "+255678647268"
+      },
+      "recording": false,
+      "number": "+255689514544",
+      "error": "not connected"
+    },
+//    "tigo1": {
+//      "type": "dongle",
+//      "level": 67,
+//      "direction": null,
+//      "label": "+255718885887",
+//      "mode": "defunct",
+//      "muted": false,
+//      "timestamp": null,
+//      "autoanswer": null,
+//      "contact": null,
+//      "recording": false
+//    },
+    "vodacom1": {
+      "type": "Dongle",
+      "level": 67,
+      "direction": null,
+      "label": "+255754885885",
+      "mode": "free",
+      "muted": false,
+      "timestamp": null,
+      "autoanswer": null,
+      "contact": null,
+      "recording": false,
+      "number": "+255754885885",
+      "error": "not connected"
+    }
+  },
+  "master": {
+    "level": 79,
+    "on_air": true,
+    "muted": false,
+    "recording": false,
+    "delay": 0,
+    "out": {
+      "level": 66,
+      "muted": false
+    },
+    "in": {
+      "level": 75,
+      "muted": false
+    }
+  },
+  "host": {
+    "level": 75,
+    "muted": false
+  },
+  "channelList": [
+    {
+      "id": "airtel1",
+      "type": "Dongle",
+      "level": 56,
+      "direction": "incoming",
+      "label": "+255689514544",
+      "mode": "ring",
+      "muted": false,
+      "timestamp": 1461056646818,
+      "autoanswer": null,
+      "contact": {
+        "name": "johannes",
+        "number": "+255678647268"
+      },
+      "recording": false,
+      "number": "+255689514544",
+      "error": "not connected"
+    },
+    {
+      "id": "vodacom1",
+      "type": "Dongle",
+      "level": 67,
+      "direction": null,
+      "label": "+255754885885",
+      "mode": "free",
+      "muted": false,
+      "timestamp": null,
+      "autoanswer": null,
+      "contact": null,
+      "recording": false,
+      "number": "+255754885885",
+      "error": "not connected"
+    },
+    {
+      "id": "tigo1",
+      "type": "Dongle",
+      "level": 56,
+      "direction": "incoming",
+      "label": "+255689514544",
+      "mode": "master",
+      "muted": false,
+      "timestamp": 1461066884306,
+      "autoanswer": null,
+      "contact": {
+        "name": "Uri Geller",
+        "number": "+255678647268"
+      },
+      "recording": false,
+      "number": "+255689514544",
+      "error": "not connected"
+    },
+  ]
 }
 
 function modeWeight(mode) {
@@ -45,7 +184,7 @@ export default function(state = initialState, action) {
     case CHANNEL_UPDATE: {
       const channels = {
         ...state.channels,
-        [action.id]: action.data, 
+        [action.id] : action.data, 
       }
       return {
         ...state, 
@@ -58,12 +197,26 @@ export default function(state = initialState, action) {
         ...state.channels,
         [action.id]: {
           ...state.channels[action.id],
-          level: action.level,
+          level : action.level,
         }, 
       }
       return {
         ...state,
         channels, 
+        channelList : channelList(channels),
+      }
+    }
+    case CHANNEL_CONTACT_UPDATE: {
+      const channels = {
+        ...state.channels,
+        [action.id]: {
+          ...state.channels[action.id],
+          contact : Object.assign({}, state.channels[action.id].contact, action.info),
+        }
+      }
+      return {
+        ...state,
+        channels,
         channelList : channelList(channels),
       }
     }
