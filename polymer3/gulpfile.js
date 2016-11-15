7e44131a423b62d42e738532497326037b806e0d
Johannes Hilden <johannes@isomorphic.co>
2016-11-15 10:32:14 +0300

    create basic gulpfile

--- polymer3/gulpfile.js
+++ polymer3/gulpfile.js
@@ -1,74 +1,32 @@
-const gulp  = require('gulp');  
-const babel = require('gulp-babel');
+const babel     = require('gulp-babel');
+const gulp      = require('gulp');
+const gulpif    = require('gulp-if');
 const vulcanize = require('gulp-vulcanize');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const historyApiFallback = require('connect-history-api-fallback');
 
-gulp.task('scripts', () => 
-  gulp.src('app/elements/**/*.js')
-    .pipe(babel({presets: ['es2015']}))
-    .pipe(gulp.dest('dist/elements/'))); 
-
-gulp.task('html', () =>  
-  gulp.src('app/elements/**/*.html')
-    .pipe(gulp.dest('dist/elements/'))); 
-
-gulp.task('css', () => 
-  gulp.src('app/elements/**/*.css')
-    .pipe(gulp.dest('dist/elements/'))); 
-
-gulp.task('vulcanize', () => {
-  return gulp.src('app/elements/elements.html')
+gulp.task('vulcanize', () => 
+  gulp.src('app/elements/elements.html')
     .pipe(vulcanize({
       stripComments: true,
       inlineScripts: true,
       inlineCss: true
     }))
-    .pipe(gulp.dest('dist/elements'));
-});
+    .pipe(gulpif('*.js', babel({
+      presets: ['es2015']
+    }))) 
+    .pipe(gulp.dest('dist/elements')));
 
-gulp.task('serve', ['css', 'scripts'], () => {
-  browserSync({
-    port: 5000,
-    notify: false,
-    logPrefix: 'PSK',
-    snippetOptions: {
-      rule: {
-        match: '<span id="browser-sync-binding"></span>',
-        fn: function(snippet) {
-          return snippet;
-        }
-      }
-    },
-    server: {
-      baseDir: ['.tmp', 'app'],
-      middleware: [historyApiFallback()]
-    }
-  });
+gulp.task('scripts', () => 
+  gulp.src('app/scripts/**/*.js')
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('dist/scripts'))); 
 
-  gulp.watch(['app/**/*.html', '!app/bower_components/**/*.html'], ['scripts', reload]);
-  gulp.watch(['app/styles/**/*.css'], ['styles', reload]);
-  gulp.watch(['app/scripts/**/*.js'], ['scripts', reload]);
-  gulp.watch(['app/images/**/*'], reload);
-});
+gulp.task('index', () =>  
+  gulp.src('app/index.html')
+    .pipe(gulp.dest('dist'))); 
 
-gulp.task('serve:dist', ['default'], () => {
-  browserSync({
-    port: 5001,
-    notify: false,
-    logPrefix: 'PSK',
-    snippetOptions: {
-      rule: {
-        match: '<span id="browser-sync-binding"></span>',
-        fn: function(snippet) {
-          return snippet;
-        }
-      }
-    },
-    server: dist(),
-    middleware: [historyApiFallback()]
-  });
-});
+gulp.task('copy', () =>
+  gulp.src([
+    'app/bower_components/{webcomponentsjs,platinum-sw,sw-toolbox,promise-polyfill,redux}/**/*'
+  ]).pipe(gulp.dest('dist/bower_components')));
 
-gulp.task('default', ['scripts', 'css', 'html', 'vulcanize']);
+gulp.task('default', ['copy', 'vulcanize', 'scripts', 'index']);
